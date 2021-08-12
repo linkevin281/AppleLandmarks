63191747162223b2ab529b8845298423e65b81ff
Kevin Lin <linkevin281@gmail.com>
2021-08-12 15:17:47 -0400

    First commit

---
+++ AppleLandmarks/AppleLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleLandmarksApp.swift
+//  AppleLandmarks
+//
+//  Created by Kevin Lin on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AppleLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
