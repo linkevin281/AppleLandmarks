fa7ca051b21a37ae65e6aa832a5c6c8ecc5b06f7
Kevin Lin <linkevin281@gmail.com>
2021-08-12 15:46:37 -0400

    Creating and Combining Views

--- AppleLandmarks/Assets.xcassets/CreatingAndCombiningViews/Complete/Landmarks/ContentView.dataset/ContentView.swift
+++ AppleLandmarks/Assets.xcassets/CreatingAndCombiningViews/Complete/Landmarks/ContentView.dataset/ContentView.swift
@@ -1,9 +1,9 @@
-//
-//  ContentView.swift
-//  AppleLandmarks
-//
-//  Created by Kevin Lin on 8/12/21.
-//
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A view showing the details for a landmark.
+*/
 
 import SwiftUI
 
@@ -13,29 +13,31 @@ struct ContentView: View {
             MapView()
                 .ignoresSafeArea(edges: .top)
                 .frame(height: 300)
-            
+
             CircleImage()
                 .offset(y: -130)
                 .padding(.bottom, -130)
-            
+
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
-                    .foregroundColor(.black)
+
                 HStack {
                     Text("Joshua Tree National Park")
                     Spacer()
                     Text("California")
                 }
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
                 Divider()
+
                 Text("About Turtle Rock")
                     .font(.title2)
                 Text("Descriptive text goes here.")
             }
-                .padding()
-            
+            .padding()
+
             Spacer()
         }
     }
