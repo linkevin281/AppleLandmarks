22d26f9a7267553bf9d03d5ba1af918c2a80e79b
Kevin Lin <linkevin281@gmail.com>
2021-08-13 15:21:30 -0400

    Draw path and shapes

---
+++ AppleLandmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  AppleLandmarks
+//
+//  Created by Kevin Lin on 8/13/21.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
+
