6e7a95fd106308839c81e6d3ae417c5ff79c8412
Kevin Lin <linkevin281@gmail.com>
2021-08-13 23:37:39 -0400

    Repo error

--- AppleLandmarks/Model/ModelData.swift
+++ AppleLandmarks/Model/ModelData.swift
@@ -10,7 +10,6 @@ import Combine
 
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
-    var hikes: [Hike] = load("hikeData.json")
 }
 
 
